e64439fc0d92e48332f78493bae82a4ca6edcb05
xynave <loic.chaligne@gmail.com>
2020-05-31 23:46:20 +0200

    rajout icone coloré footer

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 <footer class="footer">
-<i class="icon is-size-5 fab fa-facebook-square"></i>
-<i class=" icon is-size-5 fab fa-linkedin"></i>
+    <div class="centered">
+    <p > Created by Lowan Steinmetz and Loïc Chaligne</p>
+            <a href=""><i class="icon is-size-5 fab fa-facebook-square" ></i></a>
+            <a href=""><i class=" icon is-size-6 fab fa-linkedin"></i></a>
+            <a href=""><i class=" icon is-size-4 fab fa-instagram"></i></a>
+            <a href=""><i class=" icon is-size-4 fab fa-twitter"></i></a>
+          
+    </div>
+    
+
 </footer>
