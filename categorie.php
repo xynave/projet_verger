8f7cc2e692aa6f760cccff8c2d9845fd0ca424d2
xynave <loic.chaligne@gmail.com>
2020-06-25 13:24:25 +0200

    selection type relier a la variété

--- categorie.php
+++ categorie.php
@@ -4,12 +4,11 @@
                               <label class="input-group-text" for="inputGroupSelect01">Type</label>
                             </div>
                             <select class="custom-select" id="inputGroupSelect01">
-                              <option selected>Choisi ton type de produit</option>
+                              <option value="0">Choisi ton type de produit</option>
                               <option value="1">Fruit</option>
                               <option value="2">Légume</option>
                               <option value="3">Autres</option>
                             </select>
-                            <input type="button" value="CHANGE" onclick="selectFunction()" />
                           </div>
                     </div>
 
@@ -20,44 +19,51 @@
                             </div>
                             <select class="custom-select" id="inputGroupSelect02">
                               <option selected>Choisi ta variété</option>
-                              
+                             
                             </select>
                           </div>
                     </div>
                    
 
                      </br>
-                    
+                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
                      <script>
-                      var p=0;
-                      function selectFunction() {
-                        var c = document.createElement("option");
-                        
-                      
-                            document.getElementById("accueil").textContent= document.getElementById("inputGroupSelect01").options.item(0).text;
-                            //document.getElementById("profil").textContent=document.getElementById("inputGroupSelect01").selectedIndex;
-                         
-                       if( document.getElementById("inputGroupSelect01").value==='1'){
-                           if(p===0){
-                            document.getElementById("inputGroupSelect01").options.remove(0);
-                            p++;
-                           }
-
-                        
-                           if(document.getElementById("inputGroupSelect02").options[1].text)
-                          {
-                            document.getElementById("inputGroupSelect02").options[1].text="Kiwi";
-                           
-                          }else{
-                            c.text = "Kiwi";
-                            document.getElementById("inputGroupSelect02").options.add(c,1);
+                     
+                      $(document).ready(function () {
+                      $("#inputGroupSelect01").click(function () {
+                      $('#inputGroupSelect02')
+                      .find('option')
+                       .remove()
+                       .end()
+                    
+                             if($("#inputGroupSelect01").children("option:selected").val()==='1')
+                            {$('#inputGroupSelect02')
+                            .append('<option  value="Choisi_ta_variété">Choisi ta variété</option>')
+                            .append('<option value="Pomme">Pomme</option>')
+                            .append('<option value="Poire">Poire</option>')
+                           .append('<option value="Abricot">Abricot</option>')
                           }
-                            
-                            
-                       }
-                          
-                        
-                       }
+                          else if($("#inputGroupSelect01").children("option:selected").val()==='2')
+                            {$('#inputGroupSelect02')
+                            .append('<option  value="Choisi_ta_variété">Choisi ta variété</option>')
+                            .append('<option value="Courgette">Courgette</option>')
+                            .append('<option value="Carotte">Carrotte</option>')
+                           .append('<option value="Radis">Radis</option>')
+                          }
+                          else if($("#inputGroupSelect01").children("option:selected").val()==='3')
+                            {$('#inputGroupSelect02')
+                            .append('<option  value="Choisi_ta_variété">Choisi ta variété</option>')
+                            .append('<option value="Oeuf">Oeuf</option>')
+                            .append('<option value="Noix">Noix</option>')
+                          }
+                          else if($("#inputGroupSelect01").children("option:selected").val()==='0')
+                            {$('#inputGroupSelect02')
+                            .append('<option  value="Choisi_ta_variété">Choisi ta variété</option>')
+                           
+                          }
+                      });
+                       });
+                   
                        
                       
                      </script>
