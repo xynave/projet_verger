2bbb3d408126b287e10727cae8b96e625bca1cd1
xynave <loic.chaligne@gmail.com>
2020-07-11 12:20:57 +0200

    carrousel disponnible pour annonce

--- p_annonces.php
+++ p_annonces.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // On démarre la session AVANT toute chose
-$bdd = new PDO('mysql:host=localhost;dbname=leveraged;charset=utf8', 'root', 'root');
+$bdd = new PDO('mysql:host=localhost;dbname=leveraged;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 $annonces = $bdd->query('SELECT * FROM annonces');
 ?>
@@ -45,64 +45,60 @@ $annonces = $bdd->query('SELECT * FROM annonces');
           <!----====== Annonces ======---->
 
           <div class="list-group my-2">
-          <?php
-          while ($donnees = $annonces->fetch())
-          {
-
-
-
-
+            <?php
+            $compte=0;
+            while ($donnees = $annonces->fetch()) {
+              $compte=1+ $compte;
               echo '
 
 
-                <!-- noveau item-->
-                                                      <div class="card mb-3 mr-3 ml-3" >
-                                                        <div class="row no-gutters">
-                                                          <div class="col-md-4">
-                                                            <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel">
-                                                                <ol class="carousel-indicators">
-                                                                  <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
-                                                                  <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
-                                                                  <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
-                                                                </ol>
-                                                                
-                                                                <div class="carousel-inner">
-                                                                
-                                                                  <div class="carousel-item active">
-                                                                  
-                                                                    <img src="./images/1200px-Lobo.2008-12-17[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
-                                                                  </div>
-                                                                  <div class="carousel-item">
-                                                                    <img src="./images/Malus-Boskoop_organic[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
-                                                                  </div>
-                                                                  <div class="carousel-item">
-                                                                    <img src="./images/Malus-Boskoop_organic[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
-                                                                  </div>
-                                                                </div>
-                                                                <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
-                                                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                                                  <span class="sr-only">Previous</span>
-                                                                </a>
-                                                                <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next">
-                                                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                                                  <span class="sr-only">Next</span>
-                                                                </a>
-                                                              </div>
-                                                          </div>
-                                                          <div class="col-md-8">
-                                                            <div class="card-body">
+<!-- noveau item-->
+                  <div class="card mb-3 mr-3 ml-3" >
+                    <div class="row no-gutters">
+                      <div class="col-md-4">
+                        <div id="carouselExampleIndicators'.htmlspecialchars($compte).'" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleIndicators'.htmlspecialchars($compte).'" data-slide-to="0" class="active"></li>
+                              <li data-target="#carouselExampleIndicators'.htmlspecialchars($compte).'" data-slide-to="1"></li>
+                              <li data-target="#carouselExampleIndicators'.htmlspecialchars($compte).'" data-slide-to="2"></li>
+                            </ol>
+                            
+                            <div class="carousel-inner">
+                            
+                              <div class="carousel-item active">
+                              
+                                <img src="./images/1200px-Lobo.2008-12-17[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="./images/Malus-Boskoop_organic[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="./images/Malus-Boskoop_organic[1].jpg" width="50" height="180"class="d-block w-100" alt="...">
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators'.htmlspecialchars($compte).'" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators'.htmlspecialchars($compte).'" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
 
-                                                              <h5 class="card-title">' . $donnees['titre_annonce'] . '</h5>
-                                                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                                              <br>
-                                                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>  <a href="uneannonce.php" class="btn  btn-outline-dark float-right">Voir</a></p>
+                          <h5 class="card-title">' . $donnees['titre_annonce'] . '</h5>
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <br>
+                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>  <a href="uneannonce.php" class="btn  btn-outline-dark float-right">Voir</a></p>
 
-                                                            </div>
-                                                          </div>
-                                                        </div>
-                                                      </div>
-                                                      ';
-
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  ';
             }
             ?>
           </div>
@@ -160,5 +156,5 @@ $annonces = $bdd->query('SELECT * FROM annonces');
 <!--- Fin de traitement de la bdd ----->
 
 <?php
-$reponse->closeCursor(); // Termine le traitement de la requête
+$annonces->closeCursor(); // Termine le traitement de la requête
 ?>
